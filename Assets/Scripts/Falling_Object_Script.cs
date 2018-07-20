85f06c5db194b4a239fb7e92407fea99dcadec0b
Wurmie <wurmie04@gmail.com>
2018-07-19 18:55:40 -0700

    first upload

---
+++ Assets/Scripts/Falling_Object_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_Object_Script : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 7;
+        //start randomly
+        float randomXstart = Random.Range(-2.5f, 2.5f);
+        float randomYstart = Random.Range(8.0f, 12.0f);
+        transform.position = new Vector2(randomXstart, randomYstart);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move out of bounds on y axis
+        if (transform.position.y <= -5.5f)
+        {
+            MoveToTop();
+        }
+        transform.Translate(Vector2.down * Time.deltaTime * speed);
+    }
+
+    void MoveToTop()
+    {
+        //start back at the top of the screen plus new random x and y coordinate.
+        float randomNumber = Random.Range(-2.5f, 2.5f);
+        float randomY = Random.Range(8.0f, 12.0f);
+        transform.position = new Vector2(randomNumber, randomY);
+    }
+
+    //if collide with player, then move back up top
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Square")
+        {
+            MoveToTop();
+        }
+    }
+}
+
+
+
+
+
---
+++ Assets/Scripts/Player_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour {
+    public int lives;
+
+    private void Start()
+    {
+        lives = 3; 
+    }
+    // Use this for initialization
+    void Update()
+    {
+        //gameover
+        if(lives == 0)
+        {
+            Time.timeScale = 0;
+            Application.Quit();
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                    Vector2 touchPos = Camera.main.ScreenToWorldPoint(new Vector2(touch.position.x, 30));
+                    transform.position = Vector2.Lerp(transform.position, touchPos, 2.0f *Time.deltaTime);
+            }
+        }
+    }
+    //if collide with falling objects
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "triangle")
+        {
+            lives--;
+        }
+    }
+}
