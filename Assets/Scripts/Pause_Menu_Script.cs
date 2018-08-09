3527cd0235e1be02be4d6ee13778b3cdb599c519
Wurmie <wurmie04@gmail.com>
2018-08-09 01:19:53 -0700

    Made a button for pause and fixed pause and restart issues
    still some QoL stuff with pause

--- Assets/Scripts/Pause_Menu_Script.cs
+++ Assets/Scripts/Pause_Menu_Script.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause_Menu_Script : MonoBehaviour {
     //This GameObject PauseUI will represent the actual object of the Menu.   
     public GameObject PauseUI;
     private bool GamePaused = false;
-
 
 
 	// Use this for initialization
@@ -16,30 +16,24 @@ public class Pause_Menu_Script : MonoBehaviour {
         
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        //Checks if the button assigned for Pause has been pressed. Check Edit > Project Settings > Input
-        if (Input.GetButtonDown("Pause")) {
-            GamePaused = !GamePaused;
-        }
-        if (GamePaused) {
-            PauseUI.SetActive(true);
-            //Time.timeScale controls flow of the game. When 0, no time has been allocated and thus the game is paused
-            Time.timeScale = 0;
-        }
-        if (!GamePaused) {
-            PauseUI.SetActive(false);
-            Time.timeScale = 1;
-            //when Time.timeScale is 1, the game is back to flowing at a regular time flow.
-        }
+	public void PauseGame () {
+        //make pause button disappear and make pause screen appear
+        PauseUI.SetActive(true);
+        Time.timeScale = 0;
 	}
 
+    //resumes the game
     public void Resume_Game(){
-        GamePaused = false;
+        //make pause button reappear and pause screen disappear
+        PauseUI.SetActive(false);
+        Time.timeScale = 1;
     }
 
+    //restarts the game
     public void Restart() {
-        Application.LoadLevel(Application.loadedLevel); 
+        Time.timeScale = 1;
+        PauseUI.SetActive(false);
+        SceneManager.LoadScene("play");
     }
 
     public void Quit() {
